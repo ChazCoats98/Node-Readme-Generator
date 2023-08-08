c59885cdffa33d3ee293308b58b017fb0098aa2a
Chaz Coats <chazcoats2@hotmail.com>
2023-08-08 15:38:42 -0400

    Updating Inquirer

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,6 +13,23 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage 
+  ${data.usage}
+
+  ## Contribution
+  ${data.contribution}
+
+  ##GitHub
+  ${data.GitHub}
+
+  ## Email
+  ${data.email}
 
 `;
 }
