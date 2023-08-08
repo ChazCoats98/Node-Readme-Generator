a155a855c6ce6ecc0b6ab17798a408f3391e0dd0
Chaz Coats <chazcoats2@hotmail.com>
2023-08-08 11:30:00 -0400

    Adding questions arr

--- Develop/index.js
+++ Develop/index.js
@@ -2,28 +2,56 @@
 var http = require("http");
 var fs = require("fs");
 var inquirer = require("inquirer");
+var util = require("util");
+var generateMarkdown = require("./utils/generateMarkdown");
 
 
 // TODO: Create an array of questions for user input
-const questions = ["What is the title of your project?", 
-"Describe what your project was made for", 
-"What are the steps required to install your project?", 
-"Describe how to use your project", 
-"List any contribution guidelines", 
-"List any test instructions"];
-
-inquirer.prompt(questions).then((answers)=> {
-    console.log(answers);
-});
-
+const questions = [
+    {type: "input",
+    name: "title",
+    message: "What is the title of your project?"},
+    {type: "input",
+    name: "description",
+    message: "Describe what your project was made for"},
+    {type: "input",
+    name: "installation",
+    message: "What are the steps required to install your project?"},
+    {type: "input",
+    name: "usage",
+    message: "Describe how to use your project"},
+    {type: "input",
+    name: "contribution",
+    message: "List any contribution guidelines"},
+    {type: "input",
+    name: "test",
+    message: "List any test instructions"},
+    {type: "input",
+    name: "GitHub",
+    message: "What is your GitHub username?"},
+    {type: "input",
+    name: "email",
+    message: "What is your email address?"},
+    {type: "list",
+    name: "license",
+    message: "Which license would you like to use?",
+    choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "MIT License", "Boost Software License 1.0", "The Unlicense"]}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile("readme.md", )
+    fs.writeFile("readme.md", data, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("README.MD file has been generated")
+    });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    var answers = await inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
